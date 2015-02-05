c098a4beba77c6f08a2a26b4451f4e9de6fdb708
svaria <shaanvaria@MacBook-Air.local>
2015-02-05 13:12:39 -0500

    made hwtemplate more robust and updated routers

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Humanoids submission' });
+  res.render('index', { title: 'Humanoids Spring 2015' });
 });
 
 router.get('/hw1', function(req, res, next) {
-  res.render('hw1', { title: 'Homework 1' });
+  res.render('hwtemplate', { title: 'Homework 1', description: 'Here is our submission for homework 1',code: "submission/ass2-arm.zip" });
 });
+
+router.get('/hw2', function(req, res, next) {
+  res.render('hwtemplate', { title: 'Homework 2', description: 'Here is our submission for homework 2',code: "images/test.jpg" });
+});
+
 
 module.exports = router;
