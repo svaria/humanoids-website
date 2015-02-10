76a52635fd478162cf4be9a0c77d4acdc5f49040
svaria <shaanvaria@MacBook-Air.local>
2015-02-10 17:39:35 -0500

    fixed links

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Humanoids Spring 2015' });
 });
 
-router.get('/hw1', function(req, res, next) {
-  res.render('hw1', { title: 'Homework 1', code: "submission/ass2-arm.zip" });
+router.get('/hw2', function(req, res, next) {
+  res.render('hw2', { title: 'Homework 2', code: "submission/ass2-arm.zip" });
 });
 
-router.get('/hw2', function(req, res, next) {
-  res.render('hwtemplate', { title: 'Homework 2', description: 'Here is our submission for homework 2',code: "images/test.jpg" });
-});
+//router.get('/hw2', function(req, res, next) {
+//  res.render('hwtemplate', { title: 'Homework 2', description: 'Here is our submission for homework 2',code: "images/test.jpg" });
+//});
 
 
 module.exports = router;
